e5c3eac4c76a3841a9a4bb6f487805b1299ba9c0
Raúl Zavaleta <raul.zavaletazea@gmail.com>
2021-10-17 22:17:35 -0500

    Validación de datos desde el controlador

--- app/src/main/java/mx/edu/uteq/dapps/holamundo197/CurpActivity.java
+++ app/src/main/java/mx/edu/uteq/dapps/holamundo197/CurpActivity.java
@@ -1,7 +1,10 @@
 package mx.edu.uteq.dapps.holamundo197;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +26,14 @@ public class CurpActivity extends AppCompatActivity {
     private Button btnCurp;
 
     /*
+    AlertDialog es un componente
+    que permite visualizar mensaje en una
+    ventana modal que se superpone al contenido
+    de la pantlla (screen)
+     */
+    private AlertDialog.Builder alerta;
+
+    /*
     El método onCreate es el detonante o
     metodo inicial en el ciclo de vida de un
     activity, sería, en equivalencia con Java SE
@@ -34,6 +45,26 @@ public class CurpActivity extends AppCompatActivity {
 
         //Este controlador indica cual es su vista asociada
         setContentView(R.layout.activity_curp);
+
+        /*
+        Inicializamos nuestra alerta usando el contexto
+        de esta clase
+         */
+        alerta = new AlertDialog.Builder(CurpActivity.this);
+
+        /*
+        Las alertas tiene 6 métodos principales
+        1.- Titulo
+        2.- Texto (cuerpo)
+        3.- El ícono
+        4.- Botones (hasta 3)
+        5.- configuración (indeterminada, cancelable, etc)
+        6.- Que sea visible
+         */
+//        alerta.setTitle("Hola mundo");
+//        alerta.setMessage("Aquín en la alerta bien agusto...");
+//        alerta.show();
+
 
         /*
         Para indicarle al controlador (activity) cual es el
@@ -69,10 +100,65 @@ public class CurpActivity extends AppCompatActivity {
          */
         final String nombres = etNombre.getText().toString();
 
+        alerta.setTitle("En el click del boton generar curp");
+        alerta.setMessage("Diste clic a este botón");
         /*
-        Mostramos el texto en el titulo
+        Los botones se dividen en 3 posiciones
+        Positive --> acciones que agregan, editan, muestran
+        Negative --> acciones que eliminan, esconden o regresan
+        Neutral ---> acciones que no generan ningún cambio
          */
-        tvTitulo.setText("¡Hola " + nombres + "!");
+        alerta.setPositiveButton("Continuar", null);
+
+        alerta.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                /*
+                Regresar al home (MainActivity.java)
+                 */
+                startActivity(
+                        new Intent(
+                                CurpActivity.this,
+                                MainActivity.class
+                        )
+                );
+            }
+        });
+
+//        alerta.setNeutralButton("Neu", null);
+        //Evitamos que la modal se cierre desde el fondo
+        alerta.setCancelable(false);
+        alerta.setIcon(R.drawable.alerta);
+        //alerta.show(); PARA VER EL EJEMPLO DESCOMENTA ESTA LINEA
+
+        /*
+        Mostramos el texto en el titulo SI TIENE MAS DE 3 LETRAS
+         */
+
+        if (nombres.trim().length() >= 3) {
+            tvTitulo.setText("¡Hola " + nombres + "!");
+        }
+
+        else {
+            alerta.setTitle("ERROR");
+            alerta.setMessage("Nombre inválido");
+            alerta.setPositiveButton(null, null);
+            alerta.setNegativeButton(null, null);
+            alerta.setNeutralButton(
+                    "Corregir",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //Limpaimos el campo nombres
+                            etNombre.setText("");
+                            //Ponemos el cursor en este campo
+                            etNombre.requestFocus();
+                    }
+            });
+            alerta.show();
+
+        }
+
 
     }
 
